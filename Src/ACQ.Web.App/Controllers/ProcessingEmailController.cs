4c1681dce94ffc496d6572bfb0eb85a61eebadf8
shalini <pshalini.pandey@gmail.com>
2021-04-05 18:03:35 +0530

    Escalation Page
    Escalation Page

---
+++ Src/ACQ.Web.App/Controllers/ProcessingEmailController.cs
@@ -0,0 +1,57 @@
+﻿using ACQ.Web.Core.Library;
+using ACQ.Web.ExternalServices.Email;
+using Ganss.XSS;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using static ACQ.Web.App.MvcApplication;
+using ACQ.Web.App.ViewModel;
+
+namespace ACQ.Web.App.Controllers
+{
+    public class ProcessingEmailController : Controller
+    {
+        HtmlSanitizer sanitizer = new HtmlSanitizer();
+        private static string WebAPIUrl = ConfigurationManager.AppSettings["APIUrl"].ToString();
+
+        // GET: validation Check
+        [Route("validationchecker")]
+        [HandleError]
+        [SessionExpire]
+        [SessionExpireRefNo]
+        public ActionResult validationcheck()
+        {
+            IEnumerable<EscalationReportData> listdata = new List<EscalationReportData>();
+
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Clear();
+                client.BaseAddress = new Uri(WebAPIUrl);
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(mediaType: "application/json"));
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(scheme: "Basic",
+                         parameter: "GipInfoSystem" + ":" + "QmludGVzaEAxMDE");
+                HttpResponseMessage response = client.GetAsync("Escalation/GetEscalationData").Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    listdata = response.Content.ReadAsAsync<IEnumerable<EscalationReportData>>().Result;
+
+                }
+            }
+
+
+            return View(listdata);
+        }
+    }
+}
---
+++ Src/ACQ.Web.App/ViewModel/EscalationReportData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ACQ.Web.App.ViewModel
+{
+    public class EscalationReportData
+    {
+        public long Pkey { get; set; }
+        public int aon_id { get; set; }
+        public string Service_Lead_Service { get; set; }
+        public string item_description { get; set; }
+        public Nullable<System.DateTime> Date_of_Accord_of_AoN { get; set; }
+        public string Cost { get; set; }
+        public string Categorisation { get; set; }
+        public string IC_percentage { get; set; }
+        public string Trials_Required { get; set; }
+        public string TaskDescription { get; set; }
+        public Nullable<System.DateTime> completed_on { get; set; }
+        public Nullable<int> no_of_weeks { get; set; }
+        public string dap_timeline { get; set; }
+        public decimal TaskSlno { get; set; }
+        public string Responsible_Level1 { get; set; }
+        public string Responsible_Level2 { get; set; }
+        public string Responsible_Level3 { get; set; }
+        public string Responsible_Level4 { get; set; }
+        public int EscalationTime { get; set; }
+    }
+}
