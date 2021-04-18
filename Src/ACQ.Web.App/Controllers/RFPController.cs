462d9814cf81711996f6877466ffe159bbf0e320
Lalit Yadav <lalit.yadav@adequateinfosoft.com>
2021-04-18 20:23:53 +0530

    commit by lalit 18042021

---
+++ Src/ACQ.Web.App/Controllers/RFPController.cs
@@ -0,0 +1,126 @@
+﻿using ACQ.Web.Core.Library;
+using ACQ.Web.ExternalServices.Email;
+using Ganss.XSS;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using static ACQ.Web.App.MvcApplication;
+using ACQ.Web.App.ViewModel;
+using ACQ.Web.ExternalServices.SecurityAudit;
+using ACQ.Web.ViewModel.User;
+using System.Globalization;
+using System.Web.UI.WebControls;
+using System.Web.UI;
+
+namespace ACQ.Web.App.Controllers
+{
+    public class RFPController : Controller
+    {
+        // GET: RFP
+
+        HtmlSanitizer sanitizer = new HtmlSanitizer();
+        private static string WebAPIUrl = ConfigurationManager.AppSettings["APIUrl"].ToString();
+
+        public RFPController()
+        {
+            if (BruteForceAttackss.refreshcount == 0 && BruteForceAttackss.date == null)
+            {
+                BruteForceAttackss.date = System.DateTime.Now;
+                BruteForceAttackss.refreshcount = 1;
+            }
+            else
+            {
+                TimeSpan tt = System.DateTime.Now - BruteForceAttackss.date.Value;
+                if (tt.TotalSeconds <= 30)
+                {
+                    if (BruteForceAttackss.refreshcount > 20)
+                    {
+                        if (System.Web.HttpContext.Current.Session["EmailID"] != null)
+                        {
+                            IEnumerable<LoginViewModel> model = null;
+                            using (var client2 = new HttpClient())
+                            {
+                                client2.DefaultRequestHeaders.Clear();
+                                client2.BaseAddress = new Uri(WebAPIUrl);
+                                client2.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(mediaType: "application/json"));
+                                client2.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(scheme: "Basic",
+                                    parameter: "GipInfoSystem" + ":" + "QmludGVzaEAxMDE");
+                                HttpResponseMessage response = client2.GetAsync(requestUri: "Account/GetUserLoginBlock?EmailId=" + System.Web.HttpContext.Current.Session["EmailID"].ToString()).Result;
+                                if (response.IsSuccessStatusCode)
+                                {
+                                    LoginViewModel model1 = new LoginViewModel();
+                                    model = response.Content.ReadAsAsync<IEnumerable<LoginViewModel>>().Result;
+                                    if (model.First().Message == "Blocked")
+                                    {
+                                        System.Web.HttpContext.Current.Response.Redirect("/Account/Logout");
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        BruteForceAttackss.refreshcount = BruteForceAttackss.refreshcount + 1;
+                    }
+                }
+                else
+                {
+                    if (BruteForceAttackss.refreshcount > 20)
+                    {
+                        if (System.Web.HttpContext.Current.Session["EmailID"] != null)
+                        {
+                            IEnumerable<LoginViewModel> model = null;
+                            using (var client2 = new HttpClient())
+                            {
+                                client2.DefaultRequestHeaders.Clear();
+                                client2.BaseAddress = new Uri(WebAPIUrl);
+                                client2.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(mediaType: "application/json"));
+                                client2.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(scheme: "Basic",
+                                    parameter: "GipInfoSystem" + ":" + "QmludGVzaEAxMDE");
+                                HttpResponseMessage response = client2.GetAsync(requestUri: "Account/GetUserLoginBlock?EmailId=" + System.Web.HttpContext.Current.Session["EmailID"].ToString()).Result;
+                                if (response.IsSuccessStatusCode)
+                                {
+                                    LoginViewModel model1 = new LoginViewModel();
+                                    model = response.Content.ReadAsAsync<IEnumerable<LoginViewModel>>().Result;
+                                    if (model.First().Message == "Blocked")
+                                    {
+                                        System.Web.HttpContext.Current.Response.Redirect("/Account/Logout");
+
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        BruteForceAttackss.refreshcount = BruteForceAttackss.refreshcount + 1;
+                    }
+                }
+
+            }
+        }
+
+
+
+        [Route("ViewRFP")]
+        [HandleError]
+        [SessionExpire]
+        [SessionExpireRefNo]
+        public ActionResult ViewRFP()
+        {
+            return View();
+        }
+    }
+}
